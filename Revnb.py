095dffbccc908ab2d125421f1df63f580b09938a
Finix <44357910+Pangeranfinix@users.noreply.github.com>
2022-08-15 19:58:36 +0700

    Update Revnb.py

--- Revnb.py
+++ Revnb.py
@@ -1,99 +1,100 @@
-# -*- coding: utf-8 -*
-#!/usr/bin/python
-#Created By : TurgotAlp
-#Created Date = 14/04/2022
-#####################################
-import requests, re, urllib2, os, sys, codecs, random				
-from multiprocessing.dummy import Pool					     	
-from time import time as timer	
-import time
-import json				   		
-from platform import system	
-from random import sample
-from colorama import Fore								
-from colorama import Style								
-from pprint import pprint								
-from colorama import init
-from urlparse import urlparse
-import warnings
-import subprocess
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
-warnings.simplefilter('ignore',InsecureRequestWarning)
-reload(sys)  
-sys.setdefaultencoding('utf8')
-init(autoreset=True)
-##########################################################################################
-abang = '\033[31m'
-ijo = '\033[32m'
-kuning = '\033[33m'
-birtu = '\033[34m'
-ungu = '\033[35m'
-biru = '\033[36m'
-abu = '\033[37m'
-putih = '\033[0m' 
-year = time.strftime("%y")
-month = time.strftime("%m")
-#####################################
-##########################################################################################
+import requests, urllib3, re, time, random, sys, os, socket
+from colorama import Fore, Back, Style, init
+from multiprocessing.dummy import Pool
+
+init()
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+thread = 100
+tmpSites = []; ipsList = []
+outputFile = open("sr_sites.txt", "a")
+
 def logo():
-    clear = "\x1b[0m"
-    colors = [36, 32, 34, 35, 31, 37]
-
-    x = """
-  [#] Create By :: OWNER
-
-
-          < ReverseIPLookup >   < Telegram : @vxynb  >
-	     Banyuwangi Xploiter - Bandung Xploiter
-"""
-    for N, line in enumerate(x.split("\n")):
-        sys.stdout.write("\x1b[1;%dm%s%s\n" % (random.choice(colors), line, clear))
+    colors = list(vars(Fore).values())
+    os.system(["clear", "cls"][os.name == 'nt'])
+    Logo = '''
+        ______  __      ____            ____
+       / ___/ |/ /     / __ \___ _   __/  _/___
+       \__ \|   /_____/ /_/ / _ \ | / // // __ \\
+      ___/ /   /_____/ _, _/  __/ |/ // // /_/ /
+     /____/_/|_|    /_/ |_|\___/|___/___/ .___/
+     {y}Finix {w}[{g}@{w}] {y}Zonexploiter          /_/ {w}v3\n\n'''.format(g=Fore.GREEN, w=Fore.WHITE, m=Fore.MAGENTA, y=Fore.YELLOW, r=Fore.RED)
+    for line in Logo.splitlines():
+        print("".join(colors[random.randint(1, len(colors)-1)] + line.format(w=Fore.WHITE, r=Fore.RED)))
         time.sleep(0.05)
 
+def opt():
+    siteList = []
+    fileName = raw_input(" {w}[{g}+{w}] {y}the list {w}> ".format(w=Fore.WHITE, g=Fore.GREEN, y=Fore.YELLOW))
+    if os.path.exists(fileName): siteList = open(fileName, "r+").readlines()
+    else:
+        print(" {A}[{B}x{A}] {B}The list not found in current dir".format(A=Fore.WHITE, B=Fore.BLUE))
+        exit()
+    theThread = raw_input(" {w}[{g}+{w}] {y}thread {w}({y}default{w}:{y}100{w}) {w}> ".format(w=Fore.WHITE, g=Fore.GREEN, y=Fore.YELLOW))
+    if theThread == "": theThread = 100
+    if siteList == []:
+        print(" {A}[{B}x{A}] {B}Empty list".format(A=Fore.WHITE, B=Fore.BLUE))
+        exit()
+    else:
+        if len(siteList) < int(theThread): theThread = len(siteList)
+        return siteList, int(theThread)
 
-logo()
-start_raw = raw_input("\n\033[ List IPS\033[97m:~# \033[97m")
-crownes = raw_input("\033[91mthread \033[97m\033[97m:~# \033[97m")
-try:
-    with open(start_raw, 'r') as f:
-        ooo = f.read().splitlines()
-except IOError:
-    pass
-    
-try:
-    ooo = list((ooo))
-except NameError:
-    print '\033[31mOpen Your Eyes!'
-    sys.exit()
-count=0
-with open(start_raw,'r')as f:
- for line in f:
-    count+=1
-print '\x1b[91m[\x1b[92m+\x1b[91m]\x1b[92mTOTAL WEBLIST=',count
+class ReverseIp:
 
-def revsip(url):
-	try:
-		grab = requests.get('https://sonar.omnisint.io/reverse/'+url, timeout=10,headers={'User-Agent': 'Mozlila/5.0 (Linux; Android 7.0; SM-G892A Bulid/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/60.0.3112.107 Moblie Safari/537.36'})
-		if 'null' in grab.text:
-			print(kuning+'[+]' + url + abang + '>' +'Failed')
-		else:
-			result = json.loads(grab.text)
-			print(kuning+'[+]' + birtu+'IP :'+' ' +ijo + str(url) +' '+'Total Domain =>' ), len(result)
-			for domain in result: 
-				ASU = domain.replace('cpanel.','').replace('cpcalendars.','').replace('cpcontacts.','').replace('webmail.','').replace('webdisk.','').replace('hostmaster.','').replace('mail.','').replace('ns1.','').replace('ns2.','').replace('api.','').replace('ns3.','').replace('root.','').replace('autogrill.dev.','').replace('development.','').replace('.cdn.cloudflare.net.','')
-				open('Rev.txt', 'a').write('http://' + ASU + "\n")
-	except:
-			pass
+    def __init__ (self, ip):
+        self.headers = { 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0' }
+        self.result = [];
+        self.ip = ip
 
-def Main():
+    def filter ( self, site ):
+        global tmpSites
+        site = site.replace("www.", "").replace('cpanel.', '').replace('autodiscover.', '').replace('webmail.', '').replace('webdisk.', '').replace('ftp.', '').replace('cpcalendars.', '').replace('cpcontacts.', '').replace('mail.', '').replace('ns1.', '').replace('ns2.', '').replace('smtp.', '')
+        if site != "" and site not in self.result and site not in tmpSites: 
+            tmpSites.append(site)
+            self.result.append(site)
+        return 0
+
+    def sonar ( self ):
+        try:
+            response = requests.get( "https://sonar.omnisint.io/reverse/" + self.ip, headers = self.headers )
+            for site in response.json(): self.filter( site )
+            return 1
+        except: return 0
+
+    def execute (self):
+        global outputFile, tmpSites
+
+        self.sonar()
+        if len( self.result ) != 0:
+            outputFile.write("\n".join(self.result))
+            return len(self.result)
+        else: return 0
+
+def perror (url, msg): print(Back.RED+" -- "+msg+" -- \033[0m "+url)
+def psucces (url, count): print(Back.GREEN + Fore.WHITE + " -- "+str(count)+" SITES -- \033[0m "+url)
+
+def rev(url):
+    global tmpSites, outputFile, ipsList
+    if url.startswith("http://"): url = url.replace("http://", "")
+    elif url.startswith("https://"): url = url.replace("https://", "")
+    url = url.replace("\n", "").replace("\r", "").replace("/", "")
     try:
-        start = timer()
-        pp = Pool(int(crownes))
-        pp = pp.map(revsip, ooo)
-        print('TIME TAKE: ' + str(timer() - start) + ' S')
-    except:
-        pass
+        ip = socket.gethostbyname(url)
+        if ip in ipsList: print(Back.RED+" -- SAME IP -- \033[0m "+url); return 0
+        ipsList.append(ip)
+        revip = ReverseIp(ip)
+        result = revip.execute()
+        if result <= 0: perror(url, "EMPTY")
+        else: psucces(url, result)
+    except Exception as e: perror(url, "ERROR")
 
-
-if __name__ == '__main__':
-    Main()
+if __name__ == "__main__":
+    try:
+        logo(); sx = opt(); print("\n")
+        pool = Pool(sx[1])
+        pool.map(rev, sx[0])
+        pool.close()
+        pool.join()
+        print("\n {A}[{B}+{A}] {Y}Done {A}: {Y}{S} sites".format(Y=Fore.YELLOW,A=Fore.WHITE, B=Fore.BLUE, S=(str(len(tmpSites)))))
+    except KeyboardInterrupt:
+        print("\n {w}[{r}-{w}] {b}Goodbye >//< ".format(w=Fore.WHITE, r=Fore.RED, b=Fore.BLUE))
